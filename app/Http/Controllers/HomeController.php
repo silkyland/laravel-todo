7f8d6ae0a38b70699a60ab6c045952d5462401f5
Bundit Nuntates <silkyland@gmail.com>
2017-07-06 00:55:47 +0700

    B4Sleep

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,26 +6,35 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         return view('home');
     }
 
-    public function create(){
+    public function create()
+    {
         return view('create');
     }
 
-    public function store(Request $request){
+    public function store(Request $request)
+    {
+        $rules = [
+            'name' => 'required|min:3',
+            'category_id' => 'required|numeric'
+        ];
+        $this->validate($request, $rules);
         return redirect('/');
     }
 
-    public function edit($id){
+    public function edit($id)
+    {
         //
         return view('edit');
     }
 
-    public function update(Request $request, $id){
+    public function update(Request $request, $id)
+    {
         //
         return redirect('/');
     }
-
 }
